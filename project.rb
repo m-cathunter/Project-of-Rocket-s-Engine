ff4d65f18d7dcf696d9edc3e7ad29de5cb0355f7
Magdalena Kotołowska <35601753+m-cathunter@users.noreply.github.com>
2019-03-11 08:55:32 +0100

    Update project.rb

--- project.rb
+++ project.rb
@@ -1,14 +1,3 @@
-#Project of rocket
-
-#The rocket has to have:
-#1. Nose cone (A) on the start
-#2. Nozzle (N) on the end
-#3. Propellant tank - it has to start by letters PT and end by letters TP
-#4. Propellants (X) - it has to be inside of Propellant Tank, for example (PTXXXTP)
-#They can be a lot of Propellant Tanks and a lot of Propellants in Propellant Tanks
-#The rocket also can have:
-#5. Chamber for Payload (CFP) with can be placed only behind the Nose Cone
-
 # I'm asking for engine schematic and get user input
 print "Put your engine schematic here:"
 x = gets.chomp
